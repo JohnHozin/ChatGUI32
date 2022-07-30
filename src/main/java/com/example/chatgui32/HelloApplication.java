fb6eac85dc8bcec6d1012f77a42cccc72e89bab1
JohnHozin <Darktrundle727>
2022-07-30 22:09:56 +0300

    change

--- src/main/java/com/example/chatgui32/HelloApplication.java
+++ src/main/java/com/example/chatgui32/HelloApplication.java
@@ -15,6 +15,7 @@ public class HelloApplication extends Application {
         stage.setTitle("Chat32");
         stage.setScene(scene);
         stage.show();
+
     }
 
     public static void main(String[] args) {
